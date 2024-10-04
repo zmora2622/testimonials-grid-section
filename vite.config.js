d02c97784aad09f82c2a0f280a1a6d6fe42db659
Lukas <zmora2622@o2.pl>
2024-10-04 09:13:06 +0200

    build app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 export default defineConfig({
+ base: "./",
+ build: {
+  outDir: "./docs/",
+ },
  css: {
   preprocessorOptions: {
    scss: {
     api: "modern-compiler", // or "modern", "legacy"
    },
   },
- },
- build: {
-  outDir: "./docs/",
  },
 });
